9fda0f8ef37570f7e655b66b26be9966ebe29744
Diego Santos <dgo.santos89@gmail.com>
2017-03-27 22:14:48 -0300

    consultando o banco de dados e exibindo na tela

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,17 @@
 module.exports = function(app) {
 	app.get('/produtos',function(req,res){
-		res.render("produtos/lista");
+		var mysql = require('mysql');
+		var connection = mysql.createConnection({
+			host : 'localhost',
+			user : 'Usuario',
+			password : '123456',
+			database : 'casadocodigo'
+		});
+		
+		connection.query('select * from produtos', function(err, results){
+			res.render('produtos/lista',{lista:results});
+		});
+
+		connection.end();
 	});
 }
