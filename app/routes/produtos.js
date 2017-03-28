63d581786bdd9e7b077fa9a759f5ab85609cb0c1
Diego Santos <dgo.santos89@gmail.com>
2017-03-27 22:42:02 -0300

    carregamento automatico dos modulos

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,13 +1,7 @@
 module.exports = function(app) {
 	app.get('/produtos',function(req,res){
-		var mysql = require('mysql');
-		var connection = mysql.createConnection({
-			host : 'localhost',
-			user : 'Usuario',
-			password : '123456',
-			database : 'casadocodigo'
-		});
-		
+		var connection = app.infra.connectionFactory();
+
 		connection.query('select * from produtos', function(err, results){
 			res.render('produtos/lista',{lista:results});
 		});
