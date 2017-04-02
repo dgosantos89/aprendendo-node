5695ac35673b5c2ec3da13c2b34506ed87d76d89
Diego Santos <dgo.santos89@gmail.com>
2017-04-02 16:20:00 -0300

    salvando produtos no banco

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,12 +1,29 @@
 module.exports = function(app) {
-	app.get('/produtos',function(req,res){
-		var connection = app.infra.connectionFactory();
-		var ProdutosDAO = new app.infra.ProdutosDAO(connection);
 
-		ProdutosDAO.lista(function(err, results){
+	var listaProdutos = function(req,res){
+		var connection = app.infra.connectionFactory();
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+		produtosDAO.lista(function(err, results){
 			res.render('produtos/lista',{lista:results});
 		});
-
 		connection.end();
+	};
+
+	app.get('/produtos',listaProdutos);
+
+	app.get('/produtos/form',function(req,res){
+		res.render('produtos/form')
+	});
+
+	app.post('/produtos/salva',function(req,res){
+		var produto = req.body;
+
+		var connection = app.infra.connectionFactory();
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+		produtosDAO.salva(produto,function(erros,resultados){
+			res.redirect('/produtos');
+		});
+
 	});
 }
