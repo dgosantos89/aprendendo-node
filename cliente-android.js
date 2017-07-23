373c392d5e02b51a1c54875067cd3dfbeb4e201c
Diego Santos <dgo.santos89@gmail.com>
2017-07-23 15:52:12 -0300

    cliente teste para simular requisicoes em json

---
+++ cliente-android.js
@@ -0,0 +1,21 @@
+/*
+*Cliente teste para simular requisicoes no formato json
+*/
+
+var http = require('http');
+
+var configuracoes = {
+	hotsname: 'localhost',
+	port: 3000,
+	path: '/produtos',
+	headers: {
+		'Accept': 'application/json'
+	}
+};
+
+http.get(configuracoes, function(res){
+	console.log(res.statusCode);
+	res.on('data', function(body) {
+		console.log('Corpo:'+body);
+	});
+});
