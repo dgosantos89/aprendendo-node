6a3fdcf376987de39991f57b497a5cd6bb919bec
Diego Santos <dgo.santos89@gmail.com>
2017-07-23 15:53:17 -0300

    teste criado para simular um POST para cadastro de um livro no formato JSON

---
+++ cadastra-livros-terminal.js
@@ -0,0 +1,31 @@
+/*
+* Criado para simular um POST para cadastro de um livro no formato JSON
+*/
+
+var http = require('http');
+
+var configuracoes = {
+	hotsname: 'localhost',
+	port: 3000,
+	path: '/produtos',
+	method: 'post',
+	headers: {
+		'Accept': 'application/json',
+		'Content-type': 'application/json'
+	}
+};
+
+var client = http.request(configuracoes, function(res){
+	console.log(res.statusCode);
+	res.on('data', function(body) {
+		console.log('Corpo:'+body);
+	});
+});
+
+var produto = {
+	titulo: 'mais sobre node',
+	descricao: 'node, javascript e um pouco sobre http',
+	preco: 100
+};
+
+client.end(JSON.stringify(produto));
