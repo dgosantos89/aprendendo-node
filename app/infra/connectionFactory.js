e3532ebec86514e31fff04ded1de8aaf53229057
Diego Santos <dgo.santos89@gmail.com>
2017-08-19 13:17:13 -0300

    adicionando base de dados para teste

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,12 +1,22 @@
 var mysql = require('mysql');
 
 function createDBConnection() {
-	return mysql.createConnection({
-		host : 'localhost',
-		user : 'Usuario',
-		password : '123456',
-		database : 'casadocodigo'
-	});
+	if(!process.env.NODE_ENV){
+		return mysql.createConnection({
+			host : 'localhost',
+			user : 'Usuario',
+			password : '123456',
+			database : 'casadocodigo'
+		});
+	}
+	if(process.env.NODE_ENV == 'test'){
+		return mysql.createConnection({
+			host : 'localhost',
+			user : 'Usuario',
+			password : '123456',
+			database : 'casadocodigo_test'
+		});
+	}
 }
 
 module.exports = function(){
